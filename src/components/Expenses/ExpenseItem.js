12f06a17b9d18280c28047e7398b2258e45aec41
sunnyclinches44 <sunnyclinches44@gmail.com>
2021-05-26 12:42:01 +1000

    Conditional display

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,20 @@
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
   return (
-    <Card className="expense-item">
-      <ExpenseDate date ={props.date}/>
-      <div className="expense-item_description">
-        <h2>{props.title}</h2>
-        <div className="expense-item_price">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item_description">
+          <h2>{props.title}</h2>
+          <div className="expense-item_price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
